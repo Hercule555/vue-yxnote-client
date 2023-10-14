373807ee77a6bb244ba4ee5db73f5b008183eeef
Hercule555 <1076297655@qq.com>
2023-10-14 10:04:14 +0800

    Login.vue 组件功能的实现、优化

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="login">
-    <h1>{{ msg }}</h1>
+    <h1></h1>
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
           <div class="main"></div>
           <div class="form">
-            <h3>创建账户</h3>
-            <div v-show="false" class="register">
-              <input type="text" placeholder="用户名" />
-              <input type="password" placeholder="密码" />
-            </div>
-            <h3>登录</h3>
-            <div v-show="true" class="login">
-              <input type="text" placeholder="输入用户名" />
-              <input type="text" placeholder="输入密码" />
-              <div class="button">登录</div>
-            </div>
+            <h3 @click="showRegister">创建账户</h3>
+            <transition name="slide">
+              <div :class="{show: isShowRegister}" class="register">
+                <input type="text" v-model="register.username" placeholder="用户名" />
+                <input type="password" v-model="register.password" placeholder="密码" />
+                <p :class="{error: register.isError}">{{register.notice}}</p>
+                <div class="button" @click="onRegister">创建账号</div>
+              </div>
+            </transition>
+            <h3 @click="showLogin">登录</h3>
+            <transition name="slide">
+              <div :class="{show: isShowLogin}" class="login">
+                <input type="text" v-model="login.username" placeholder="输入用户名" />
+                <input type="password" v-model="login.password" placeholder="输入密码" />
+                <p :class="{error: login.isError}">{{login.notice}}</p>
+                <div class="button" @click="onLogin">登录</div>
+              </div>
+            </transition>
           </div>
         </div>
       </div>
@@ -29,9 +36,80 @@ export default {
   name: "Login",
   data() {
     return {
-      msg: "this is login page",
+      isShowLogin: true,
+      isShowRegister: false,
+      login: {
+        username: 'hunger',
+        password: '123456',
+        notice: '请输入用户名和密码',
+        isError: false
+      },
+      register: {
+        username: '',
+        password: '',
+        notice: '请记住账户和密码',
+        isError: false
+      }
     };
   },
+  methods: {
+    showLogin() {
+      this.isShowLogin = true
+      this.isShowRegister = false
+    },
+    showRegister() {
+      this.isShowRegister = true
+      this.isShowLogin = false
+    },
+    onRegister() {
+      let result1 = this.validUsername(this.register.username)
+      if(!result1.isValid) {
+        this.register.isError = true
+        this.register.notice = result1.notice
+        return
+      }
+      let result2 = this.validPassword(this.register.password)
+      if(!result2.isValid) {
+        this.register.isError = true
+        this.register.notice = result2.notice
+        return
+      }
+      this.register.isError = false
+      this.register.notice= '创建成功'
+      console.log('开始注册,用户名是:',this.register.username, '密码是：', this.register.password )
+      
+    },
+    onLogin() {
+      let result1 = this.validUsername(this.login.username)
+      if(!result1.isValid) {
+        this.login.isError = true
+        this.login.notice = result1.notice
+        return
+      }
+      let result2 = this.validPassword(this.login.password)
+      if(!result2.isValid) {
+        this.login.isError = true
+        this.login.notice = result2.notice
+        return
+      }
+      this.login.isError = false
+      this.login.notice= '登录成功'
+      console.log('开始登录,用户名是:',this.login.username, '密码是：', this.login.password )
+    },
+    validUsername(username) {
+      return {
+        isValid: /^[a-zA-Z_0-9\u4e00-\u9fa5]{3,15}$/.test(username),
+        notice: '用户名必须是3~15个字符, 限于字母、数字、下划线'
+      }
+    },
+    validPassword(password) {
+      return {
+        isValid: /^.{6,16}$/.test(password),
+        notice: '密码长度必须是6~16个字符'
+      }
+    }
+    
+  }
 };
 </script>
 
@@ -78,6 +156,7 @@ h1 {
   .form {
     width: 270px;
     border-left: 1px solid #ccc ;
+    overflow: hidden;
 
     h3 {
       padding: 10px 20px;
@@ -98,10 +177,17 @@ h1 {
       background-color: #2bb964;
       border-radius: 4px;
       margin-top: 15px;
+      cursor: pointer;
     }
     .login,.register {
-      padding: 10px 20px;
+      padding: 0px 20px;
       border-top: 1px solid #eee;
+      height: 0px;
+      overflow: hidden;
+      transition: height .4s;
+      &.show {
+        height: 190px;
+      }
 
       input {
         // display: block;
@@ -129,7 +215,7 @@ h1 {
     }
     .login { 
         border-top: 0;
-      }
+    }
   }
 }
 </style>
