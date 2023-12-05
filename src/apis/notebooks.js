941115dcb78e0787e526abbe3dbae1158d3df601
Hercule555 <1076297655@qq.com>
2023-12-05 21:23:24 +0800

    对笔记本页面添加：增、删、改。功能

--- src/apis/notebooks.js
+++ src/apis/notebooks.js
@@ -1,5 +1,5 @@
 import request from '@/helpers/request'
-
+import {friendlyDate} from '@/helpers/date-util'
 const URL =  {
   GET: '/notebooks',
   ADD: '/notebooks',
@@ -8,8 +8,22 @@ const URL =  {
 }
 
 export default {
-  getALL() {
-    return request(URL.GET)
+  getAll() {
+     return new Promise((resolve,reject) => {
+       request(URL.GET)
+        .then(res => {
+          console.log(res)
+          res.data = res.data.sort((notebook1,notebook2) => notebook1.createdAt < notebook2.createdAt ? 1 : -1)
+          res.data.forEach(notebook => {
+             notebook.friendlyCreatedAt = friendlyDate(notebook.createdAt)            
+          });
+          resolve(res)
+        }).catch(err => {
+          reject(err)
+        })
+       
+    })
+    
   },
   updateNotebook(notebookid, {title = ''} = {title:''}) {
     return request(URL.UPDATE.replace(':id', notebookid), 'PATCH', { title })
--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="notebook-list">
     <header>
-      <a href="#" class="btn"><i class="iconfont icon-plus"></i>新建笔记本</a>
+      <a href="#" class="btn" @click.prevent="onCreate"
+        ><i class="iconfont icon-plus"></i>新建笔记本</a
+      >
     </header>
     <main>
-      <h3 class="layout">笔记本列表(10)</h3>
+      <h3 class="layout">笔记本列表({{ notebooks.length }})</h3>
       <div class="book-list">
-        <a href="#" class="notebook">
+        <router-link
+          v-for="notebook in notebooks"
+          :key="notebook.id"
+          to="/note/1"
+          class="notebook"
+        >
           <div>
-            <span class="iconfont icon-notebook"></span>笔记本标题1
-            <span>3</span> <span class="action">编辑</span>
-            <span class="action">删除</span>
-            <span class="date">3天前</span>
+            <span class="iconfont icon-notebook"></span>{{ notebook.title }}
+            <span>{{ notebook.counts }}</span>
+            <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
+            <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
+            <span class="date">{{notebook.friendlyCreatedAt}}</span>
           </div>
-        </a>
-
-        <a href="#" class="notebook">
-          <div>
-            <span class="iconfont icon-notebook"></span>笔记本标题2
-            <span>1</span> <span class="action">编辑</span>
-            <span class="action">删除</span>
-            <span class="date">5天前</span>
-          </div>
-        </a>
+        </router-link>
       </div>
     </main>
     <!-- <ul>
@@ -38,15 +37,52 @@
 
 <script>
 import Auth from "@/apis/auth";
-import Notebooks from '@/apis/notebooks'
+import Notebooks from "@/apis/notebooks";
+import {friendlyDate} from '@/helpers/date-util'
 
 // window.Notebooks = Notebooks
 
 export default {
   data() {
     return {
-      msg: "笔记本列表",
+      notebooks: [],
     };
+  },
+  methods: {
+    onCreate() {
+      let title = window.prompt("创建笔记本页");
+      if (title.trim() === "") {
+        alert("笔记本名不能为空");
+        return;
+      }
+      Notebooks.addNotebook({ title }).then((res) => {
+        console.log(res);
+        res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
+        this.notebooks.unshift(res.data)
+         alert(res.msg)
+      });
+    },
+    onEdit(notebook) {
+      console.log('edit', notebook)
+      let title = window.prompt('修改标题', notebook.title)
+      Notebooks.updateNotebook(notebook.id, {title})
+       .then(res => {
+         console.log(res)
+         notebook.title = title
+         alert(res.msg)
+       })
+    },
+    onDelete(notebook) {
+      let isConfirm = window.confirm('确定删除吗？')
+      if(isConfirm) {
+        Notebooks.deleteNotebook(notebook.id)
+         .then(res => {
+           console.log(res)
+           this.notebooks.splice(this.notebooks.indexOf(notebook),1)
+           alert(res.msg)
+         })
+      }
+    },
   },
   created() {
     Auth.get_info().then((res) => {
@@ -54,12 +90,13 @@ export default {
         this.$router.push({ path: "login" });
       }
     });
+    Notebooks.getAll().then((res) => {
+      this.notebooks = res.data;
+    });
   },
 };
 </script>
 
 <style lang="less" scoped>
-
 @import url(../assets/css/notebook-list);
-
 </style>
