90ff4d965de9f37f49d0a405807b212f540a818b
Hercule555 <1076297655@qq.com>
2023-12-11 15:13:52 +0800

    引入element-ui

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -17,9 +17,13 @@
           <div>
             <span class="iconfont icon-notebook"></span>{{ notebook.title }}
             <span>{{ notebook.counts }}</span>
-            <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
-            <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
-            <span class="date">{{notebook.friendlyCreatedAt}}</span>
+            <span class="action" @click.stop.prevent="onEdit(notebook)"
+              >编辑</span
+            >
+            <span class="action" @click.stop.prevent="onDelete(notebook)"
+              >删除</span
+            >
+            <span class="date">{{ notebook.friendlyCreatedAt }}</span>
           </div>
         </router-link>
       </div>
@@ -38,7 +42,7 @@
 <script>
 import Auth from "@/apis/auth";
 import Notebooks from "@/apis/notebooks";
-import {friendlyDate} from '@/helpers/date-util'
+import { friendlyDate } from "@/helpers/date-util";
 
 // window.Notebooks = Notebooks
 
@@ -50,38 +54,57 @@ export default {
   },
   methods: {
     onCreate() {
-      let title = window.prompt("创建笔记本页");
-      if (title.trim() === "") {
-        alert("笔记本名不能为空");
-        return;
-      }
-      Notebooks.addNotebook({ title }).then((res) => {
-        console.log(res);
-        res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
-        this.notebooks.unshift(res.data)
-         alert(res.msg)
-      });
+      this.$prompt("请输入标题", "创建笔记本页面", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        inputPattern: /^.{1,35}$/,
+        inputErrorMessage: "笔记本标题不能为空,且不超过30个字符",
+      })
+        .then(({ value }) => {
+          return Notebooks.addNotebook({ title: value });
+        })
+        .then((res) => {
+          res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt);
+          this.notebooks.unshift(res.data);
+          this.$message.success(res.msg)
+          
+        })
+
     },
     onEdit(notebook) {
-      console.log('edit', notebook)
-      let title = window.prompt('修改标题', notebook.title)
-      Notebooks.updateNotebook(notebook.id, {title})
-       .then(res => {
-         console.log(res)
-         notebook.title = title
-         alert(res.msg)
-       })
+      let title = "";
+      this.$prompt("请输入新标题", "修改笔记本", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        inputPattern: /^.{1,30}$/,
+        inputValue: notebook.title,
+        inputErrorMessage: "笔记本标题不能为空,且不超过30个字符",
+      })
+        .then(({ value }) => {
+          title = value;
+          return Notebooks.updateNotebook(notebook.id, { title: value });
+        })
+        .then((res) => {
+          notebook.title = title;
+          this.$message.success(res.msg)
+          
+        })
+        
     },
     onDelete(notebook) {
-      let isConfirm = window.confirm('确定删除吗？')
-      if(isConfirm) {
-        Notebooks.deleteNotebook(notebook.id)
-         .then(res => {
-           console.log(res)
-           this.notebooks.splice(this.notebooks.indexOf(notebook),1)
-           alert(res.msg)
-         })
-      }
+      this.$confirm("此操作将永久删除该笔记, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          return Notebooks.deleteNotebook(notebook.id)
+        })
+        .then((res) => {
+          this.notebooks.splice(this.notebooks.indexOf(notebook), 1);
+          this.$message.success(res.msg)
+        })
+        
     },
   },
   created() {
--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 import configBaseURL from "./config-baseURL"
+import { Message } from "element-ui"
+
 
 console.log(configBaseURL)
 
@@ -11,29 +13,39 @@ export default function request(url, type = 'GET', data = {}) {
   return new Promise((resolve, reject) => {
     let option = {
       url,
-      method:type,
+      method: type,
       validateStatus(status) {
         return (status >= 200 && status < 300) || status === 400
       }
     }
-    if(type.toLowerCase() === 'get') {
+    if (type.toLowerCase() === 'get') {
       option.params = data
     } else {
       option.data = data
     }
     axios(option).then(res => {
-      if(res.status === 200) {
+      if (res.status === 200) {
         resolve(res.data)
       } else {
-        console.log(res.data)
-        reject(res.data) 
+        reject(res.data)
+
+        // console.log(error)
+        console.log(res.data) 
+        Message({
+          type: 'error', 
+          message: res.data.msg
+        })
       }
     }).catch(err => {
-      console.log({msg:'网络异常'})
-      reject({msg: '网络异常'})
+      Message({
+        type: 'eror',
+        message: '网络异常'
+      })
+
+      console.log({ msg: '网络异常' })
+      reject({ msg: '网络异常' })
     })
-  
-  })    
+
+  })
 
 }
- 
